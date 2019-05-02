b098daf41e77d27446c9eee68b90b131e6178b22
Mike <47514894+mpg33@users.noreply.github.com>
2019-05-01 17:49:14 -0700

    Add files via upload

--- SQL_HW.sql
+++ SQL_HW.sql
@@ -71,6 +71,76 @@ JOIN film f
 ON fa.film_id = f.film_id
 GROUP BY f.title;
 
-SELECT COUNT(title)
-FROM 
+SELECT title, (
+SELECT COUNT(*) FROM inventory
+WHERE film.film_id = inventory.film_id)
+FROM film
+WHERE title = "Hunchback Impossible";
+
+SELECT * FROM customer;
+
+SELECT c.last_name, SUM(p.amount) 
+FROM customer c
+JOIN payment p
+ON c.customer_id = p.customer_id
+GROUP BY c.last_name;
+
+SELECT * FROM film_actor;
+
+SELECT title
+FROM film
+WHERE title LIKE 'K%' OR title LIKE 'Q%'
+AND title IN (
+SELECT title
+FROM film
+WHERE language_id = 1);
+
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id IN (
+SELECT actor_id
+FROM film_actor
+WHERE film_id IN(
+SELECT film_id
+FROM film
+WHERE title = 'Alone Trip'));
+
+SELECT * FROM city;
+
+SELECT cu.first_name, cu.last_name, cu.email
+FROM customer cu
+JOIN address a
+ON (cu.address_id = a.address_id)
+JOIN city ct
+ON (ct.city_id = a.city_id)
+JOIN country co
+ON (co.country_id = ct.country_id)
+WHERE co.country = 'Canada'
+;
+
+SELECT * FROM film_category;
+
+SELECT title, description 
+FROM film
+WHERE film_id IN (
+SELECT film_id FROM film_category
+WHERE category_id IN (
+SELECT category_id FROM category
+WHERE name = 'Family'))
+;
+
+SELECT * FROM rental;
+
+SELECT f.title, COUNT(rental_id) 
+FROM rental r
+JOIN inventory i
+ON (r.inventory_id = i.inventory_id)
+JOIN film f
+ON (i.film_id = f.film_id)
+GROUP BY f.title
+ORDER BY 'Times Rented' DESC
+;
+
+
+
 
